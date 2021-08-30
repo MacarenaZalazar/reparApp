285611fa614d931db9d972ee6005253b981f85df
MacarenaZalazar <macarena.zalazar@gmail.com>
2021-08-30 16:56:01 -0300

    searchbar added

---
+++ client/src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+
+const Searchbar = () => {
+    const [input, setInput] = useState('')
+    const [select, setSelect] = useState('')
+
+    const handleChange  = (e) => {
+        setInput(e.target.value)
+        console.log('input on change')
+    }
+    const handleSelect = (e) => {
+        setSelect(e.target.value)
+        console.log('select on change')
+    }
+    const onClick = () => {
+        const filter = [select, input]
+    }
+
+
+    return (
+        <div>
+            <label>¿Qué necesitas?</label>
+            <select name="oficios" handleChange={handleSelect}>
+                <option value=""></option>
+            </select>
+            <label>¿Dónde?</label>
+            <input type="text" value={input} onChange={handleChange} />
+            <button onClick={onClick}>Buscá!</button>
+        </div>
+    );
+};
+
+export default Searchbar;
