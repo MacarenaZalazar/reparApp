d49f252a579a309b008032db1aa075ac1e81c148
Matias Morrone <83367721+MatiasMorrone@users.noreply.github.com>
2021-09-01 12:36:43 -0300

    components details created

--- client/src/components/finalUserDetails/finalUserDetails.jsx
+++ client/src/components/finalUserDetails/finalUserDetails.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
 
 export default function finalUserDetails(props) {
   const finalUserID = props.match.params.id;
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch("ActionQuetraigaporIddetallesdeUsuarioFinal(finalUserID)");
+  }, []);
+
+  const FinalUser = useSelector((state) => state.finalUserDetail);
 
   return (
     <div>
-      <h1>Saas</h1>
+      <h1>{FinalUser.userName}</h1>
+      <h2>{FinalUser.lastName}</h2>
+      <h4>{FinalUser.name}</h4>
+      <img src={FinalUser.image} alt="" />
+      <p>{FinalUser.zone}</p>
+      <h4>{FinalUser.score}</h4>
+      <p>{FinalUser.phone}</p>
+      <p>{FinalUser.mail}</p>
     </div>
   );
 }
