7b457d3f2d1ab3e96ae2d9c0c10178baf6e3dbcf
Gabriel Villegas <villegasnano11@gmail.com>
2021-09-01 16:39:07 -0300

    NavBar creado y renderizado en App. SearchBar dentro del Nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
-import "./App.css";
 import { Route, Switch } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import technicUserDetails from "./components/technicUserDetails/technicUserDetails";
-import Searchbar from "./components/Searchbar/Searchbar";
 import finalUserDetails from "./components/finalUserDetails/finalUserDetails";
-import DisplayFilters from './components/DisplayFilters/DisplayFilters';
+import DisplayFilters from "./components/DisplayFilters/DisplayFilters";
 import FormTechnicUser from "./components/FormTechnicalUser/FormTechnicUser";
 import FormUser from "./components/FormUser/FormUser";
-
+import NavBar from "./containers/NavBar/NavBar";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Route>
-        <Searchbar />
+        <NavBar />
       </Route>
-      <Route path='/home'>
-        <DisplayFilters/>
+      <Route path="/home">
+        <DisplayFilters />
       </Route>
       <Route exact path="/finalUserDetails:id" component={finalUserDetails} />
       <Route
@@ -24,11 +22,11 @@ function App() {
         path="/technicUserDetails:Id"
         component={technicUserDetails}
       />
-      <Route exact path='/userForm'>
-        <FormUser/>
+      <Route exact path="/userForm">
+        <FormUser />
       </Route>
-      <Route exact path='/technicForm'>
-        <FormTechnicUser/>
+      <Route exact path="/technicForm">
+        <FormTechnicUser />
       </Route>
     </div>
   );
