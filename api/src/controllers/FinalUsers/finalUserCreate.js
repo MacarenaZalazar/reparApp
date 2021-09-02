69899584cf49cde6631ceb09f2cae0e98ff1c158
sastiam <sfranciach@gmail.com>
2021-09-02 11:57:49 -0500

    se agrego transacciones al crear usuario final

---
+++ api/src/controllers/FinalUsers/finalUserCreate.js
@@ -0,0 +1,40 @@
+const FinalUser = require("../../models/FinalUser");
+const User = require("../../models/User");
+
+const finalUserCreate = async (req, res, next) => {
+  const UserSession = await User.startSession();
+  UserSession.startTransaction();
+  const user = req.body;
+  try {
+    // await UserSession.withTransaction(async () => {
+    //   //Crea el usuario base y es asignado a una variable para obtener el ID
+
+    // });
+    const newUser = await User.create(
+        [{
+          name: user.name,
+          lastName: user.lastName,
+          mail: user.mail,
+          userName: user.username,
+          password: user.password,
+        }],
+        { session: UserSession }
+      );
+
+      await FinalUser.create(
+        [{ user: newUser._id, zone: user.zone, score: user.score }],
+        { session: UserSession }
+      );
+    await UserSession.commitTransaction();
+    UserSession.endSession();
+
+    res.send('Se creo correctamente el usuario final')
+
+  } catch (error) {
+    await UserSession.abortTransaction();
+    UserSession.endSession();
+    next(error);
+  }
+};
+
+module.exports = finalUserCreate;
--- api/src/controllers/FinalUsers/finalUserModifier.js
+++ api/src/controllers/FinalUsers/finalUserModifier.js
@@ -3,7 +3,11 @@ const FinalUser = require('../../models/FinalUser')
 const finalUserModifier = async (req, res, next) => {
   const { id } = req.params;
   try {
-    
+    const fuFUser = await FinalUser.findByIdAndUpdate(id, req.body, {strict: true});
+    if(fuFUser) {
+      res.send('Data actualizada correctamente')
+    }
+    next({message: 'Usuario no encontrado', status: 404})
   } catch (err) {
     next(err);
   }
