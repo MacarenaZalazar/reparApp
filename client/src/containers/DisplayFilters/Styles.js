1fb4e3db8c66a808a1824a455031e72ec1cbb099
Macarena Zalazar <82382979+MacarenaZalazar@users.noreply.github.com>
2021-09-02 11:47:39 -0300

    css changes2

--- client/src/containers/DisplayFilters/Styles.js
+++ client/src/containers/DisplayFilters/Styles.js
@@ -8,5 +8,9 @@ export const StyledDiv = styled.div`
     background-color: transparent;
     width: 25%;
     font-size: 15px;
+    margin: 50px;
+    border-radius: 15px;
+    box-shadow: rgba(181, 129, 108, 0.8) 0px 8px 34px -25px;
+    background-color: white;
 
 `;
