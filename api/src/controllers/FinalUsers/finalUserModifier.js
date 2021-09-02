3856f2556a0575f275e93f89721f55f641670231
sastiam <sfranciach@gmail.com>
2021-09-02 09:20:21 -0500

    se removio node_modules de carpeta base

--- api/src/controllers/FinalUsers/finalUserModifier.js
+++ api/src/controllers/FinalUsers/finalUserModifier.js
@@ -1,7 +1,9 @@
+const FinalUser = require('../../models/FinalUser')
+
 const finalUserModifier = async (req, res, next) => {
   const { id } = req.params;
   try {
-    res.send("Soy modify User Final");
+    
   } catch (err) {
     next(err);
   }
