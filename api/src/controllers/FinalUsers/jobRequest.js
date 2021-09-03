6639ebe948568523481eb180e58793937bd36e7b
bordato87 <bordato87@gmail.com>
2021-09-03 11:57:56 -0300

    jobRequest

--- api/src/controllers/FinalUsers/jobRequest.js
+++ api/src/controllers/FinalUsers/jobRequest.js
@@ -1,25 +1,66 @@
-const postNewRequest = async (req,res,next) => {
-    const {user} = req.query
-    const {jobType, zone} = req.body
-    try {
-        res.send('estoy en POST /finalUsers/newRequest')
-    } catch (error) {
-        next(error)
-    }
-}
+const workOrders = require("../../models/workOrders");
 
+
+const postNewRequest = async (req,res,next) => {
+    const workOrdersSession = await workOrders.startSession();
+    const {userFinal} = req.query;
+    const {title, description, workImage, workType} = req.body;
+    try {
+        await workOrdersSession.withTransaction(async () => {
+            await workOrders.create({
+                title,
+                description,
+                userFinal,
+                workImage,
+                workType
+            });
+        });
+        workOrdersSession.endSession();
+        res.send('Se creo correctamente la orden');
+    } catch (error) {
+        workOrdersSession.endSession();
+        next({message: error?.message, status:404});
+    }
+};
+
+const requestModifier = async (req,res,next) => {
+    const workOrdersSession = await workOrders.startSession();
+    const {id} = req.params
+    const {title, description, workImage, workType} = req.body;
+    try {
+        await workOrdersSession.withTransaction(async () => {
+            await workOrders.findByIdAndUpdate(id, {
+                title,
+                description,
+                workImage,
+                workType
+            });
+        });
+        workOrdersSession.endSession();
+        res.send('Orden actualizada correctamente');
+    } catch (error) {
+        workOrdersSession.endSession();
+        next({message: error?.message, status:404});
+    }
+};
 
 const deleteRequest = async (req,res,next) => {
-    const {id} = req.params
+    const workOrdersSession = await workOrders.startSession();
+    const {id} = req.params;
     try {
-        res.send('estoy en DELETE /request/:id')
+        await workOrdersSession.withTransaction(async () => {
+            await workOrders.findByIdAndDelete(id);
+        });
+        workOrdersSession.endSession();
+        res.send('Orden eliminada correctamente');
     } catch (error) {
-        next(error)
+        workOrdersSession.endSession();
+        next({message: error?.message, status:404});
     }
-
-}
+};
 
 module.exports = {
     postNewRequest,
+    requestModifier,
     deleteRequest
 }
