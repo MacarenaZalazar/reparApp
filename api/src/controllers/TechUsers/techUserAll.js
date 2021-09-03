2a7389e4bed5d14b7ff0215c6601dba74cf0c512
Gabriel Villegas <villegasnano11@gmail.com>
2021-09-03 15:35:35 -0300

    Creacion ruta modificar user final

--- api/src/controllers/TechUsers/techUserAll.js
+++ api/src/controllers/TechUsers/techUserAll.js
@@ -1,12 +1,12 @@
-const UsersT = require('../../models/TechUser');
+const UsersT = require("../../models/TechUser");
 
 const techUserAll = async (req, res, next) => {
-    try {
-        const getAll = await UsersT.find({}).populate({path: "user"});
-        res.status(200).send(getAll)
-    } catch (error) {
-        next(error)
-    }
-}
+  try {
+    const getAll = await UsersT.find({}).populate({ path: "user" });
+    res.status(200).send(getAll);
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = techUserAll;
