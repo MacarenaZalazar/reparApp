720449718d1603718fd3264cf2ca52c00610ad04
Matias Morrone <83367721+MatiasMorrone@users.noreply.github.com>
2021-09-03 17:22:38 -0300

    rutas corregidas

--- api/src/routes/techUsers.js
+++ api/src/routes/techUsers.js
@@ -18,6 +18,6 @@ router.get("/", techUserAll);
 router.get("/:id", techUsersDetails);
 router.put("/:id", techUserModifier);
 router.post("/", techUserCreate);
-router.delete("/", deleteUserT);
+router.delete("/:id", deleteUserT);
 
 module.exports = router;
