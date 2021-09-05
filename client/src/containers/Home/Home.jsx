964d658aa5b80c303cd1edcf651d43253114ebd8
Gabriel Villegas <villegasnano11@gmail.com>
2021-09-05 04:31:49 -0300

    Redux funcionando

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import DisplayFilters from '../DisplayFilters/DisplayFilters';
-import TechnicUsers from '../TechnicUsers/TechnicUsers';
-import { StyledDiv } from './Styles';
+import React from "react";
+import DisplayFilters from "../DisplayFilters/DisplayFilters";
+import TechnicUsers from "../TechnicUsers/TechnicUsers";
+import { StyledDiv } from "./Styles";
+
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getTechUsersAll } from "../../redux/actions/techUsers/index";
 
 const Home = () => {
-    return (
-        <StyledDiv>
-            <DisplayFilters/>
-            <TechnicUsers/>
-        </StyledDiv>
-    );
+  const dispatch = useDispatch();
+  const techUsers = useSelector((state) => state.techUsers);
+
+  useEffect(() => {
+    dispatch(getTechUsersAll());
+    console.log("estoy en home:", techUsers);
+  }, []);
+  return (
+    <StyledDiv>
+      <DisplayFilters />
+      <TechnicUsers />
+    </StyledDiv>
+  );
 };
 
 export default Home;
--- client/src/containers/TechnicUsers/TechnicUsers.jsx
+++ client/src/containers/TechnicUsers/TechnicUsers.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
-import TechnicUser from '../../components/TechnicUser/TechnicUser';
-import { useSelector } from 'react-redux';
-import {technicUsers} from '../../utils/mockData'
-import {StyledDiv} from './Styles'
+import React from "react";
+import TechnicUser from "../../components/TechnicUser/TechnicUser";
+import { useSelector } from "react-redux";
+// import { technicUsers } from "../../utils/mockData";
+import { StyledDiv } from "./Styles";
 
 const TechnicUsers = () => {
-    //const {technicUsers} = useSelector(state => state)
-    return (
-        <StyledDiv>
-            {technicUsers.map((t, idx) => {
-                return <TechnicUser key={idx} name={t.name}  lastName={t.lastName} user={t.user}
-                image={t.image} score={t.score} workZones={t.workZones} jobTypes={t.jobTypes} />
-            })}
-        </StyledDiv>
-        
-    );
+  const techUsers = useSelector((state) => state.techUsers);
+  return (
+    <StyledDiv>
+      {techUsers.map((t, idx) => {
+        return (
+          <TechnicUser
+            key={idx}
+            name={t.user.name}
+            lastName={t.user.lastName}
+            user={t.user.userName}
+            image={t.image}
+            score={t.score}
+            workZones={t.workZones}
+            jobTypes={t.jobTypes}
+          />
+        );
+      })}
+    </StyledDiv>
+  );
 };
 
 export default TechnicUsers;
