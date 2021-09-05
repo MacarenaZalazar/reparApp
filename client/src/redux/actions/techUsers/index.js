34fe29eaa1a32d8eb8e9193191764b6f5f4eb91a
Gabriel Villegas <villegasnano11@gmail.com>
2021-09-05 18:10:57 -0300

    Arreglo en /technicUserDetails

--- client/src/redux/actions/techUsers/index.js
+++ client/src/redux/actions/techUsers/index.js
@@ -1,10 +1,14 @@
 import axios from "axios";
 import { TECH_USERS_URL } from "../../../utils/constants";
-import { GET_TECH_USERS_ALL, GET_TECH_USERS_BY_ID, GET_TECH_USERS_BY_JOB_ZONE } from "./constantsTechUsers";
+import {
+  GET_TECH_USERS_ALL,
+  GET_TECH_USERS_BY_ID,
+  GET_TECH_USERS_BY_JOB_ZONE,
+} from "./constantsTechUsers";
 
 export function getTechUsersAll() {
   return async function (dispatch) {
-    console.log('llegué a la action')
+    console.log("llegué a la action");
     try {
       let techUsers = await axios.get(TECH_USERS_URL);
       return dispatch({
@@ -17,11 +21,13 @@ export function getTechUsersAll() {
   };
 }
 
-export function getTechUsersById(id){
-  console.log('llegué a la action')
+export function getTechUsersById(id) {
+  console.log("llegué a la action");
   return async function (dispatch) {
+    console.log(`${TECH_USERS_URL}/${id}`);
     try {
       let techUser = await axios.get(`${TECH_USERS_URL}/${id}`);
+      console.log(techUser);
       return dispatch({
         type: GET_TECH_USERS_BY_ID,
         payload: techUser.data,
@@ -32,18 +38,19 @@ export function getTechUsersById(id){
   };
 }
 
-export function getTechUsersByJobAndZone(jobType, zone){
-  console.log('llegué a la action')
-    return async function (dispatch) {
-      try {
-        let techUsers = await axios.get(`${TECH_USERS_URL}?jobTypes=${jobType}&workZones=${zone}`);
-        return dispatch({
-          type: GET_TECH_USERS_BY_JOB_ZONE,
-          payload: techUsers.data,
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
+export function getTechUsersByJobAndZone(jobType, zone) {
+  console.log("llegué a la action");
+  return async function (dispatch) {
+    try {
+      let techUsers = await axios.get(
+        `${TECH_USERS_URL}?jobTypes=${jobType}&workZones=${zone}`
+      );
+      return dispatch({
+        type: GET_TECH_USERS_BY_JOB_ZONE,
+        payload: techUsers.data,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 }
