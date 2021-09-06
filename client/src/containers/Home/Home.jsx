63be71967b62469f157ac7b32172fa543bf4deaa
Macarena Zalazar <82382979+MacarenaZalazar@users.noreply.github.com>
2021-09-06 12:57:41 -0300

    Update Home.jsx

--- client/src/containers/Home/Home.jsx
+++ client/src/containers/Home/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import DisplayFilters from "../DisplayFilters/DisplayFilters";
 import TechnicUsers from "../TechnicUsers/TechnicUsers";
 import { StyledDiv } from "./Styles";
-
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getTechUsersAll } from "../../redux/actions/techUsers/index";
@@ -11,7 +10,6 @@ const Home = () => {
   const dispatch = useDispatch();
   const {techUsers} = useSelector(state => state)
   useEffect(() => {
-    dispatch(getTechUsersAll());
   }, []);
       return ( 
         <StyledDiv>
