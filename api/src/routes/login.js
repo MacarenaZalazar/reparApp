811e0991af5a21a967e09c6229e71033f5e178bd
Matias Morrone <83367721+MatiasMorrone@users.noreply.github.com>
2021-09-07 15:58:04 -0300

    middlewares aplicados

--- api/src/routes/login.js
+++ api/src/routes/login.js
@@ -3,6 +3,8 @@ const router = Router();
 
 const login = require("../controllers/Login");
 
+const { isuserFinal, isuserTech } = require("../middlewares");
+
 router.post("/", login);
 
 module.exports = router;
