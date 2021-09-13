77e7dfda9a5bbfc739689b521361bd24a5e78abf
Matias Morrone <83367721+MatiasMorrone@users.noreply.github.com>
2021-09-13 09:58:10 -0300

    cn

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,6 +1,6 @@
 // const BASE_URL = "http://localhost:3001/";
-// const BASE_URL = "https://api-pf-050921.herokuapp.com/";
-const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3001"
+const BASE_URL = "https://api-pf-050921.herokuapp.com/";
+// const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3001"
 const TECH_USERS_URL = `${BASE_URL}/techUsers`;
 
 // url admin
@@ -16,7 +16,7 @@ const FINAL_USER_URL = `${BASE_URL}/finalUsers`;
 //url request
 const REQUEST_URL = `${BASE_URL}/request`;
 
-const LOGIN_URL = `${BASE_URL}/login`
+const LOGIN_URL = `${BASE_URL}/login`;
 
 module.exports = {
   TECH_USERS_URL,
