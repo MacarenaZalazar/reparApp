527433ad14b30f7d2c19f60c143003586400e95e
Matias Morrone <83367721+MatiasMorrone@users.noreply.github.com>
2021-09-13 10:02:06 -0300

    r2

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:3001/";
-const BASE_URL = "https://api-pf-050921.herokuapp.com/";
+const BASE_URL = "https://api-pf-050921.herokuapp.com";
 // const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3001"
 const TECH_USERS_URL = `${BASE_URL}/techUsers`;
 
