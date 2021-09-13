4f5707c19505356a5626f4fac8189e439f4ec51d
bordato87 <bordato87@gmail.com>
2021-09-13 14:57:56 -0300

    enviar mail automatico cuando se crea usuario

--- api/src/controllers/FinalUsers/finalUserCreate.js
+++ api/src/controllers/FinalUsers/finalUserCreate.js
@@ -4,6 +4,7 @@ const { SECRET } = process.env;
 const jwt = require("jsonwebtoken");
 const Role = require("../../models/roles");
 const User = require("../../models/User");
+const nodemailer = require("nodemailer");
 
 const finalUserCreate = async (req, res, next) => {
   const session = await FinalUser.startSession();
@@ -23,6 +24,31 @@ const finalUserCreate = async (req, res, next) => {
 
       const token = jwt.sign({ id: newUser._id }, SECRET, {
         expiresIn: 86400, // 24 hours
+      });
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.email",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "reparapp.proyecto.final@gmail.com",
+          pass: "Henry12345",
+        },
+      });
+      var mailOptions = {
+        from: "reparapp.proyecto.final@gmail.com",
+        to: `${user.mail}`,
+        subject: "Bienvenido a ReparApp",
+        text: "Le damos la bienvenida a la ReparApp",
+        html: `<h2>ReparApp</h2>
+        <h3>${user.name} te damos la bienvenida a ReparApp</h3>
+        <h3>Ya podes crear pedidos de trabajo y contactarte con los mejores profesionales</h3>`,
+      };
+
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          return res.status(500).send(err.message);
+        }
       });
       res.send({ token });
     });
--- api/src/controllers/TechUsers/techUserCreate.js
+++ api/src/controllers/TechUsers/techUserCreate.js
@@ -3,6 +3,7 @@ const UserController = require("../Users");
 const { SECRET } = process.env;
 const jwt = require("jsonwebtoken");
 const Role = require("../../models/roles");
+const nodemailer = require("nodemailer");
 
 const techUserCreate = async (req, res, next) => {
   const TechUserSession = await TechUser.startSession();
@@ -19,6 +20,31 @@ const techUserCreate = async (req, res, next) => {
       });
       const token = jwt.sign({ id: newUser._id }, SECRET, {
         expiresIn: 86400, // 24 hours
+      });
+      const transporter = nodemailer.createTransport({
+        service: "gmail",
+        host: "smtp.gmail.email",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "reparapp.proyecto.final@gmail.com",
+          pass: "Henry12345",
+        },
+      });
+      var mailOptions = {
+        from: "reparapp.proyecto.final@gmail.com",
+        to: `${req.body.mail}`,
+        subject: "Bienvenido a ReparApp",
+        text: "Le damos la bienvenida a la ReparApp",
+        html: `<h2>ReparApp</h2>
+        <h3>${req.body.name} te damos la bienvenida a ReparApp</h3>
+        <h3>Ya podes ofrecer tus servicios para dale la mejor solución a los clientes</h3>`,
+      };
+
+      transporter.sendMail(mailOptions, (err, info) => {
+        if (err) {
+          return res.status(500).send(err.message);
+        }
       });
       res.send({ token });
     });
