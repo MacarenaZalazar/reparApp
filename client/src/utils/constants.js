56028b9bb0b49f99e16fb3b1e3615c34c56c082e
bordato87 <bordato87@gmail.com>
2021-09-13 18:02:59 -0300

    contactoFront

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "http://localhost:3001/";
-const BASE_URL = "https://api-pf-050921.herokuapp.com";
+const BASE_URL = "http://localhost:3001";
+// const BASE_URL = "https://api-pf-050921.herokuapp.com";
 // const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:3001"
 const TECH_USERS_URL = `${BASE_URL}/techUsers`;
 
@@ -18,6 +18,8 @@ const REQUEST_URL = `${BASE_URL}/request`;
 
 const LOGIN_URL = `${BASE_URL}/login`;
 
+const CONTACT_URL = `${BASE_URL}/contact`;
+
 module.exports = {
   TECH_USERS_URL,
   ADMIN_URL,
@@ -25,4 +27,5 @@ module.exports = {
   FINAL_USER_URL,
   REQUEST_URL,
   LOGIN_URL,
+  CONTACT_URL,
 };
