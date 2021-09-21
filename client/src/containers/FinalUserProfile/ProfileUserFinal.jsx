2d65a068b4fda8a7d0990aec4bd29bd5c3007d91
Gabriel Villegas <villegasnano11@gmail.com>
2021-09-21 02:09:38 -0300

    estilos a usuarioFinal restan muchos detalles

--- client/src/containers/FinalUserProfile/ProfileUserFinal.jsx
+++ client/src/containers/FinalUserProfile/ProfileUserFinal.jsx
@@ -1,14 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CardUserFinal from "../../components/PerfilUserFinal/CardUserFinal";
 //import UpDateUserFinal from "../../components/PerfilUserFinal/UpDateUserFinal";
 import { useDispatch, useSelector } from "react-redux";
 import { getFinalUsersById } from "../../redux/actions/finalUser/index";
-import { ContainerDiv } from "./Styles";
 import { useMemo } from "react";
+import { TiArrowLeftThick } from "react-icons/ti";
+import { Link } from "react-router-dom";
+
+import { ProfileDiv, StyledDiv, ButtonsDiv } from "./Styles";
 
 function ProfileUserFinal() {
   const userString = window.sessionStorage.getItem("user");
   const users = JSON.parse(userString);
+
+  const [back, setBack] = useState(false);
+
+  const changeBack = () => {
+    setBack(!back);
+  };
 
   let config = useMemo(() => {
     return {
@@ -16,8 +25,8 @@ function ProfileUserFinal() {
         "x-access-token": users && users.token,
       },
     };
-  }, [users]) 
-  
+  }, [users]);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -28,34 +37,54 @@ function ProfileUserFinal() {
 
   console.log(user);
   return (
-    <>
-    <ContainerDiv>
-      {user.user ? (
-        <div className="container">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm">
-                {
-                  <CardUserFinal
-                    name={user.user.name}
-                    lastname={user.user.lastName}
-                    tell={user.user.phone}
-                    mail={user.user.mail}
-                    username={user.user.userName}
-                    zone={user.zone}
-                    state={user.user.state}
-                    img={user.user.image}
-                  />
-                }
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <p>Cargando...</p>
-      )}
-    </ContainerDiv>
-    </>
+    <StyledDiv className="container">
+      <ButtonsDiv back={back}>
+        <Link onClick={changeBack} className="link" to="/home">
+          <TiArrowLeftThick className="icon" />
+          <p>Volver a Home</p>
+        </Link>
+
+        {/* <Link
+              onMouseEnter={changeFlagNewOrder}
+              onMouseOut={changeFlagNewOrder}
+              className="link"
+              to="/newWorkOrder"
+            >
+              New Work Order
+              {flagNewOrder ? (
+                <p>
+                  <TiArrowLeftThick />
+                </p>
+              ) : (
+                <p>Nueva Orden</p>
+              )}
+            </Link>
+            <Link
+              // onMouseEnter={changeFlag}
+              // onMouseOut={changeFlag}
+              className="link"
+              to="/modificarPerfilC"
+            >
+              Modificar Perfil
+            </Link> */}
+      </ButtonsDiv>
+      <ProfileDiv>
+        {user.user ? (
+          <CardUserFinal
+            name={user.user.name}
+            lastname={user.user.lastName}
+            tell={user.user.phone}
+            mail={user.user.mail}
+            username={user.user.userName}
+            zone={user.zone}
+            state={user.user.state}
+            img={user.user.image}
+          />
+        ) : (
+          <p>Cargando...</p>
+        )}
+      </ProfileDiv>
+    </StyledDiv>
   );
 }
 
--- client/src/containers/FinalUserProfile/Styles.js
+++ client/src/containers/FinalUserProfile/Styles.js
@@ -1,4 +1,46 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+
+export const StyledDiv = styled.div`
+  width: 100%;
+  height: calc(100vh - 10.75rem);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  /* 
+  background-position: center left;
+  background-size: 28%;
+  background-repeat: no-repeat; */
+`;
+
+export const ProfileDiv = styled.div`
+  height: 100%;
+`;
+export const ButtonsDiv = styled.div`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  .link {
+    transition: 0.2s ease;
+    transform: translateX(${(back) => (back ? "5rem" : "0rem")});
+
+    p {
+      color: #0a122aff;
+    }
+    .icon {
+      bottom: 0;
+      left: 0;
+
+      color: #0a122aff;
+      background-color: #f06449ff;
+      font-size: 5rem;
+      border-radius: 50%;
+      padding: 1rem;
+    }
+  }
+`;
+
 export const ContainerDiv = styled.div`
-    heigh: 100%
-`
+  heigh: 100%;
+`;
