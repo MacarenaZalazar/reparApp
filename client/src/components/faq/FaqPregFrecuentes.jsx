7ce7549c8522733f7aa88af9b92ec1e8b10b4b06
Guillermo Agustín Borquez <agustinborquez1999@hotmail.com>
2021-09-20 22:58:37 -0300

    FAQs terminados

--- client/src/components/faq/FaqPregFrecuentes.jsx
+++ client/src/components/faq/FaqPregFrecuentes.jsx
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react'
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
 // import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-function Faq(props) {
-  return (
-    <div>
-      <Accordion>
+function FaqPregFrecuentes(props) {
+    return (
+        <div>
+             <Accordion>
         <AccordionSummary
         //   expandIcon={<ExpandMoreIcon />}
           aria-controls="panel1a-content"
@@ -63,8 +63,8 @@ function Faq(props) {
           </Typography>
         </AccordionDetails>
       </Accordion>
-    </div>
-  );
+        </div>
+    )
 }
 
-export default Faq;
+export default FaqPregFrecuentes
