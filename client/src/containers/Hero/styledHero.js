df3005ef097f2eebe902b5d58f4df19e341a734c
Guillermo Agustín Borquez <agustinborquez1999@hotmail.com>
2021-09-23 15:54:24 -0300

    Se termino el SearBar,

--- client/src/containers/Hero/styledHero.js
+++ client/src/containers/Hero/styledHero.js
@@ -13,7 +13,15 @@ export const StyledDiv = styled.div`
   background-position: center left;
   background-size: 28%;
   background-repeat: no-repeat;
-`;
+  @media screen and (max-width: 768px) {
+    background-position: center top;
+    background-size: 40%;
+  }
+  @media screen and (max-width: 480px) {
+    background-position: center top;
+    background-size: 60%;
+  }
+  `;
 export const HeroDiv = styled.div`
   width: 100%;
 `;
