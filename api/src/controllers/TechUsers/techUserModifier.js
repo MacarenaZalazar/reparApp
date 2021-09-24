2806f456af424b31a0fec11e24597c529b0c69c9
Matias Morrone <83367721+MatiasMorrone@users.noreply.github.com>
2021-09-24 12:59:48 -0300

    Un

--- api/src/controllers/TechUsers/techUserModifier.js
+++ api/src/controllers/TechUsers/techUserModifier.js
@@ -4,22 +4,35 @@ const User = require("../../models/User");
 const techUserModifier = async (req, res, next) => {
   const session = await UsersT.startSession();
   const { id } = req.params;
-  const { name, lastName, image, phone, state, password, workZones, jobTypes, price} = req.body;
-  
+  const {
+    name,
+    lastName,
+    image,
+    phone,
+    state,
+    password,
+    workZones,
+    jobTypes,
+    price,
+  } = req.body;
+
   try {
     await session.withTransaction(async () => {
-
-      const {user} = await UsersT.findByIdAndUpdate(id, { workZones, jobTypes, price: "$ " + price});
+      const { user } = await UsersT.findByIdAndUpdate(id, {
+        workZones,
+        jobTypes,
+        price: parseInt(price),
+      });
       await User.findByIdAndUpdate(user, {
         name,
         lastName,
         image,
         phone,
-        state
+        state,
       });
-      if(password){
+      if (password) {
         const pass = await User.encryptPassword(password);
-        await User.findByIdAndUpdate(user, {password: pass});
+        await User.findByIdAndUpdate(user, { password: pass });
       }
     });
     session.endSession();
